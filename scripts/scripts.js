7eecd15c66a6247e1197fb90007c7a083ccddb15
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2021-04-24 12:34:38 -0400

    Add Everglades
    Added Everglades
    Cleaned up directory
    Fixed style issues

---
+++ scripts/scripts.js
@@ -0,0 +1,60 @@
+// Start Back To Top Button //
+$(document).ready(function(){
+  // Get the button
+  toTopButton = document.getElementById("backToTopBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction(toTopButton)};
+});
+
+function scrollFunction(toTopButton) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// End Back To Top Button //
+
+// Start Slideshow Functions //
+var slideIndex = 1;
+timer = null;
+$(document).ready(function(){
+  showDivs(slideIndex);
+});
+
+function plusDivs(n) {
+  clearTimeout(timer);
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  clearTimeout(timer);
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("demo");
+
+  if (n > x.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" btn-hovered", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " btn-hovered";
+
+  timer = setTimeout('plusDivs('+(1)+')', 10000); // Change image every 2 seconds
+}
+// End Slideshow Functions //
