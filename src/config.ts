cad91f43db1bb861cfd3b37d9e186a74cc01e7c0
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2022-10-13 15:46:14 -0400

    Move site to Astro

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Josh Gracie Web Resume';
+export const SITE_DESCRIPTION = 'Online web resumé for Joshua Gracie.';
+export const SITE_IMAGE = '/PlaceHolder.png'
