08a31142462b76ca375714b2e0c6a4469b7e0d12
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2022-10-13 16:57:37 -0400

    Added New Project

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Josh Gracie Web Resume';
+export const SITE_TITLE = 'Josh Gracie Web Resumé';
 export const SITE_DESCRIPTION = 'Online web resumé for Joshua Gracie.';
 export const SITE_IMAGE = '/PlaceHolder.png'
