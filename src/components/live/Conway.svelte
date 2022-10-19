0d4532bc65be2740b4f1b2c0a30d9dee7bd396f1
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2022-10-19 13:01:56 -0400

    New blogs

---
+++ src/components/live/Conway.svelte
@@ -0,0 +1,187 @@
+<script>
+    import { onMount, tick } from 'svelte';
+
+    class ConwayCell {
+        constructor(alive, td){
+            this.alive = alive;
+            this.td = td;
+        }
+    }
+
+    let gridSize = 32;
+    let aliveInitChance = 0.2;
+    let delay = 500;
+    let gridParent = null;
+    let width = 500;
+    let cellWidth = 0;
+    let play = true;
+    let conwayGrid = [...Array(gridSize)].map(e => Array(gridSize));
+    let isDrag = false;
+
+    // Mouse drag for table
+    const beginDrag = () => {
+        isDrag = true
+    }
+    
+    const endDrag = () => {
+        isDrag = false
+    }
+
+    const toggle = (r, c) => {
+        conwayGrid[r][c].alive = !conwayGrid[r][c].alive
+    }
+    
+    const mouseHandler = (r, c) => (e) => {
+        if (isDrag || e.type === 'mousedown') {
+            toggle(r, c)
+        }
+    }
+
+    onMount(() => {
+        document.addEventListener("DOMContentLoaded", function() {
+            gridParent = document.getElementById('conway-grid');
+            gridParent.style.height = gridParent.clientWidth;
+            cellWidth = width / gridSize;
+        });
+
+        playConway();
+    });
+
+    async function playConway(){
+        // Only play when play is true
+        if(!play)
+        {
+            setTimeout(playConway, delay);
+            return;
+        }
+
+        // Generate temporary grid
+        let temp = [...Array(gridSize)].map(e => Array(gridSize))
+
+        // Loop over the grid
+        for(let row = 0; row < gridSize; row++){
+            for(let col = 0; col < gridSize; col++){
+                // Count living and dead neighbors
+                let count = countNeighbors(row,col);
+
+                if(!conwayGrid[row][col].alive){
+                    // Dead cell logic
+                    if(count == 3)
+                        temp[row][col] = true;
+                    else
+                        temp[row][col] = false;
+                }
+                else{
+                    // Live cell logic
+                    if(count < 2 || count > 3)
+                        temp[row][col] = false; 
+                    else
+                        temp[row][col] = true;   
+                }                
+            }
+        }
+
+        // Replace old grid with new grid
+        // Prevents updates to alive prop from affecting calculations for other cells
+        for(let row = 0; row < gridSize; row++){
+            for(let col = 0; col < gridSize; col++){
+                conwayGrid[row][col].alive = temp[row][col];
+            }
+        }
+
+        await tick();
+        
+        setTimeout(function(){
+            playConway();
+        }, delay);
+    }
+
+    function countNeighbors(i,j){
+        let count = 0;
+        if(i != 0 && j != 0 && conwayGrid[i-1][j-1].alive)
+            count++;
+        if(i != 0 && conwayGrid[i-1][j].alive)
+            count++;
+        if(i != 0 && j != gridSize - 1 && conwayGrid[i-1][j+1].alive)
+            count++;
+        if(j != 0 && conwayGrid[i][j-1].alive)
+            count++;
+        if(j != 0 && i != gridSize - 1 && conwayGrid[i+1][j-1].alive)
+            count++;
+        if(i != gridSize - 1 && j != gridSize - 1 && conwayGrid[i+1][j+1].alive)
+            count++;
+        if(i != gridSize - 1 && conwayGrid[i+1][j].alive)
+            count++;
+        if(j != gridSize - 1 && conwayGrid[i][j+1].alive)
+            count++;
+
+        return count;
+    }
+
+    function shuffleConway(){
+        for(let row = 0; row < gridSize; row++){
+            for(let col = 0; col < gridSize; col++){
+                let alive = Math.random() < aliveInitChance ? true : false;
+                conwayGrid[row][col].alive = alive;
+            }
+        }
+    }
+
+    function resetConway(){
+        for(let row = 0; row < gridSize; row++){
+            for(let col = 0; col < gridSize; col++){
+                conwayGrid[row][col].alive = false;
+            }
+        }
+    }
+
+    function genGridItem(i,j){
+        let alive = Math.random() < aliveInitChance ? true : false;
+        conwayGrid[i][j] = new ConwayCell(alive, `con-${i}-${j}`);
+    }
+
+</script>
+
+<svelte:window on:mousedown={beginDrag} on:mouseup={endDrag} />
+<!-- <div class="text-5xl font-bold inline">Conway's <div class="inline text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-yellow-500">Game of Life</div></div> -->
+<div class="toolbar space-x-1">
+    <div class="tooltip" data-tip={play ? "Pause": "Play"}>
+        <button class="btn btn-square" on:click={() => play = !play}>
+            {#if !play}
+                <i class="fa-solid fa-play"></i>
+            {:else}
+                <i class="fa-solid fa-pause text-xl"></i>
+            {/if}
+        </button>
+    </div>
+    <div class="tooltip" data-tip="Randomize">
+        <button class="btn btn-square" on:click={() => shuffleConway()}>
+            <i class="fa-solid fa-shuffle text-xl"></i>
+        </button>
+    </div>
+    <div class="tooltip" data-tip="Reset">
+        <button class="btn btn-square" on:click={() => resetConway()}>
+            <i class="fa-solid fa-rotate-right text-xl"></i>
+        </button>
+    </div>
+    <div class="tooltip" data-tip="Github Repo">
+        <button class="btn btn-square" href="">
+            <i class="fa-brands fa-github text-xl"></i>
+        </button>
+    </div>
+</div>
+<div id="conway-grid">
+    <table class='conway-grid-inner' id='conway-grid-inner' style='height:{width}px; width:{width}px'>
+    {#each Array(gridSize) as _ , i}
+        <tr>
+        {#each Array(gridSize) as _, j}
+            {#await genGridItem(i,j) then item}
+            <td id='con-{i}-{j}' class='{conwayGrid[i][j].alive ? 'alive' : ''}' style='height:{cellWidth}px; width:{cellWidth}px' on:mousedown={mouseHandler(i, j)} on:mouseenter={mouseHandler(i, j)}>
+
+            </td>
+            {/await}
+        {/each}
+        </tr>
+    {/each}
+    </table>
+</div>
--- src/pages/blog/post1.md
+++ src/pages/blog/post1.md
@@ -1,17 +1,50 @@
 ---
 layout: "../../layouts/PostLayout.astro"
-title: "Demo Post 1"
-description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-pubDate: "Sep 10 2022"
-heroImage: "/PlaceHolder.png"
+title: "Creating a Realtime Face Recognizer"
+description: "Tutorial on how to create a realtime face recognizer using Python and OpenCV."
+pubDate: "Oct 13 2022"
+heroImage: "/FaceRecognizer.gif"
+tags: [ "computer vision", "machine learning" ]
 ---
+
+Ready to dable in the power of computer vision? In this tutorial, we will be learning how to create a realtime face detection and reconition system using Python and OpenCV. All you'll need for this project is:
+
+- A code editor (I used VS Code)
+- Python 3.9
+- OpenCV 2 (for Python)
+- A working video camera for your computer
+
+## Background
+
+Before we get started, a few quick recaps on computer vision (feel free to skip this if you're up to speed). Computer vision is the process of creating algorithms to gain a high-level
+understanding of visual inputs such as images and videos. There are many algorithms that have been created throughout the years with one of the most popular set of algorithms being
+convolutional neural networks (CNNs). These algorithms work by using mathematical convolutions to learn the features of an image automatically in order to classify them.
+
+However, let's say you want to track an object in a image (such as a face for instance). How can you track and classify an object at the same time?
+
+## Creating the recognizer
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae ultricies leo integer malesuada nunc vel risus commodo viverra. Adipiscing enim eu turpis egestas pretium. Euismod elementum nisi quis eleifend quam adipiscing. In hac habitasse platea dictumst vestibulum. Sagittis purus sit amet volutpat. Netus et malesuada fames ac turpis egestas. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Varius sit amet mattis vulputate enim. Habitasse platea dictumst quisque sagittis. Integer quis auctor elit sed vulputate mi. Dictumst quisque sagittis purus sit amet.
 
+### Detection basics
+
 Morbi tristique senectus et netus. Id semper risus in hendrerit gravida rutrum quisque non tellus. Habitasse platea dictumst quisque sagittis purus sit amet. Tellus molestie nunc non blandit massa. Cursus vitae congue mauris rhoncus. Accumsan tortor posuere ac ut. Fringilla urna porttitor rhoncus dolor. Elit ullamcorper dignissim cras tincidunt lobortis. In cursus turpis massa tincidunt dui ut ornare lectus. Integer feugiat scelerisque varius morbi enim nunc. Bibendum neque egestas congue quisque egestas diam. Cras ornare arcu dui vivamus arcu felis bibendum. Dignissim suspendisse in est ante in nibh mauris. Sed tempus urna et pharetra pharetra massa massa ultricies mi.
+
+### Creating a dataset
 
 Mollis nunc sed id semper risus in. Convallis a cras semper auctor neque. Diam sit amet nisl suscipit. Lacus viverra vitae congue eu consequat ac felis donec. Egestas integer eget aliquet nibh praesent tristique magna sit amet. Eget magna fermentum iaculis eu non diam. In vitae turpis massa sed elementum. Tristique et egestas quis ipsum suspendisse ultrices. Eget lorem dolor sed viverra ipsum. Vel turpis nunc eget lorem dolor sed viverra. Posuere ac ut consequat semper viverra nam. Laoreet suspendisse interdum consectetur libero id faucibus. Diam phasellus vestibulum lorem sed risus ultricies tristique. Rhoncus dolor purus non enim praesent elementum facilisis. Ultrices tincidunt arcu non sodales neque. Tempus egestas sed sed risus pretium quam vulputate. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Fringilla urna porttitor rhoncus dolor purus non. Amet dictum sit amet justo donec enim.
 
+### Training the recognizer
+
 Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Tortor posuere ac ut consequat semper viverra. Tellus mauris a diam maecenas sed enim ut sem viverra. Venenatis urna cursus eget nunc scelerisque viverra mauris in. Arcu ac tortor dignissim convallis aenean et tortor at. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Egestas tellus rutrum tellus pellentesque eu. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Id donec ultrices tincidunt arcu. Id cursus metus aliquam eleifend mi.
+
+## Testing the recognizer
+
+```python
+def get_started():
+    pass()
+```
+
+## Conclusion
 
 Tempus quam pellentesque nec nam aliquam sem. Risus at ultrices mi tempus imperdiet. Id porta nibh venenatis cras sed felis eget velit. Ipsum a arcu cursus vitae. Facilisis magna etiam tempor orci eu lobortis elementum. Tincidunt dui ut ornare lectus sit. Quisque non tellus orci ac. Blandit libero volutpat sed cras. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin gravida. Egestas integer eget aliquet nibh praesent tristique magna.
