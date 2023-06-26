ab70ed731f68b4de9ad3dff22cb5459a08f5d8d0
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2023-06-26 08:11:10 -0400

    Fix packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jgracie52.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), preact(), react()]
 });
