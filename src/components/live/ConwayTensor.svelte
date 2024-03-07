254eade8bb49035907efadb42434366096dba2f3
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2024-03-07 18:19:07 -0500

    Conway Tensorflow
    Reimplemented conway's game of life in Tensorflow JS

---
+++ src/components/live/ConwayTensor.svelte
@@ -0,0 +1,132 @@
+<script>
+    // Code for this is adapted from the following source:
+    // nrudakov - ConwayLife - https://github.com/nrudakov/ConwayLife/blob/master/ConwayLife.py
+
+    import { onMount } from 'svelte';
+    import FormSideBar from './FormSideBar.svelte';
+    // Importing the tensorflow.js library
+    import * as tf from "@tensorflow/tfjs"
+
+    let population;
+    let width = 0;
+    let height = 0;
+    let resolution = 10;
+    let speed = 1;
+    let backend;
+    let interval;
+
+
+    async function playConway(){
+        // This function runs the game of life simulation
+        // It takes the current population tensor and returns the next generation
+
+        // Create a copy of the population tensor
+        let newPopulation = population.clone().toFloat();
+
+        // Create a kernel for convolution
+        let kernel = tf.ones([3, 3, 1, 1]);
+
+        // Perform the convolution
+        let convolvedPopulation = tf.conv2d(newPopulation, kernel, 1, 'same');
+
+        let neighbors = tf.sub(convolvedPopulation, newPopulation);
+
+        // Apply the rules of the game of life
+        // If a cell has 3 neighbors, it becomes alive
+        // If a cell has 2 neighbors, it stays the same
+        // If a cell has less than 2 or more than 3 neighbors, it dies
+
+        let wasAlive = tf.equal(newPopulation, 1);
+        let twoLiveNeighbors = tf.equal(neighbors, 2);
+        let threeLiveNeighbors = tf.equal(neighbors, 3);
+        let finalPop = tf.logicalOr(threeLiveNeighbors, tf.logicalAnd(wasAlive, twoLiveNeighbors));
+
+        // Update the population tensor
+        population = finalPop.toFloat();
+
+        // Render the population tensor to the canvas
+        tf.browser.toPixels(population, document.getElementById('canvas'));
+    }
+
+    // On mount, initialize the canvas
+    onMount(() => {
+        // Get the size of the wrapper element
+        const wrapper = document.getElementById('wrapper');
+        width = Math.round(wrapper.clientWidth / resolution);
+        height = Math.round(wrapper.clientHeight / resolution);
+
+        let blank = tf.zeros([height, width]);
+        tf.browser.toPixels(blank, document.getElementById('canvas'));
+
+        // Setup the population tensor
+        // This represents the population of cells in the game of life
+        // All cells are initialized to 0
+        population = tf.randomUniform([height, width, 1], 0, 1, tf.int32);
+        population = population.round().toInt();
+        tf.browser.toPixels(population, document.getElementById('canvas'));
+
+        backend = tf.getBackend();
+
+        // Run the game of life simulation
+        interval = setInterval(playConway, 1000 / speed);
+        //playConway();
+    });
+
+    function updateResolution(newResolution){
+        // This function updates the resolution of the canvas
+        // It also updates the population tensor to match the new resolution
+
+        resolution = newResolution;
+
+        clearInterval(interval);
+
+        const wrapper = document.getElementById('wrapper');
+        width = Math.round(wrapper.clientWidth / resolution);
+        height = Math.round(wrapper.clientHeight / resolution);
+
+        let blank = tf.zeros([height, width]);
+        tf.browser.toPixels(blank, document.getElementById('canvas'));
+
+        population = tf.randomUniform([height, width, 1], 0, 1, tf.int32);
+        population = population.round().toInt();
+        tf.browser.toPixels(population, document.getElementById('canvas'));
+
+        interval = setInterval(playConway, 1000 / speed);
+    }
+
+    function updateSpeed(newSpeed){
+        // This function updates the speed of the game of life simulation
+        // It changes the interval at which the simulation is run
+
+        speed = newSpeed;
+
+        clearInterval(interval);
+        interval = setInterval(playConway, 1000 / speed);
+    }
+</script>
+
+<div class="flex flex-column w-full h-full">
+    <div class="w-full h-full max-h-[100vh] overflow-y-scroll p-4">
+        <div style="position:relative" class="w-full h-full" id="wrapper">
+            <canvas id="canvas" style="image-rendering: pixelated;" class="rounded-md h-full w-full"></canvas>
+        </div>
+    </div>
+    <FormSideBar>
+        <article class="prose mb-4">
+            <h2>Instructions</h2>
+            <p>This is a simple implementation of Conway's Game of Life using TensorFlow.js</p>
+            <p>The game is controlled using a simple 2D convolution to sum the living neighbors and then tensor math to set the state of the new population.</p>
+            <p>Use the sliders to control the speed (FPS) and resolution (size of the population) of the simulation.</p>
+            <p>NOTE: Set your resolution and speed carefully. If your backend is not WebGL or if you do not have a powerful GPU, high speed and a lower resolution value will result in long compute time and possible crashing.</p>
+        </article>
+
+        <p>Current backend: {backend}</p>
+        <label for="species">Speed (FPS)</label>
+        <input class="range range-xs range-primary" type="range" id="species" name="species" min="1" max="60" step="1" value={speed} on:input={e => updateSpeed(e.target.value)}>
+        <label for="population" class="block">{speed}</label>
+
+        <label for="species">Resolution (lower value is higher res)</label>
+        <input class="range range-xs range-primary" type="range" id="species" name="species" min="1" max="10" value={resolution} on:input={e => updateResolution(e.target.value)}>
+        <label for="population" class="block">{resolution}</label>
+    </FormSideBar>
+</div>
---
+++ src/pages/blog/conwaytensor.md
@@ -0,0 +1,94 @@
+---
+layout: "../../layouts/PostLayout.astro"
+title: "Conway's Game of Life in Tensorflow"
+description: "A reimplementation of Conway's Game of Life with TensorFlowJS."
+pubDate: "March 6 2024"
+heroImage: "/conwaytensor.png"
+tags: [ "cellular automata", "live demo" ]
+badge: "Demo"
+---
+
+In my previous post, I implemented Conway's Game of Life using Svelte. In that implementation, I used a 2D array to store the state of each cell and updated the state of each cell based on the state of its neighbors. Using that array, I was able to update an HTML table to display the state of each cell.
+
+While that implementation certainly worked, it would not be very efficient for larger grids. In this post, I will be reimplementing Conway's Game of Life using TensorFlowJS. This will allow me to use the GPU to perform the calculations, which should be much faster than using a 2D array and updating an HTML table.
+
+## The Grid
+
+In order to implement Conway's Game of Life, we need to set up a grid to represent the state of each cell. In TensorFlowJS, we can use a 2D tensor to represent the grid. Each cell will be represented by a 1 or 0, where 1 represents a live cell and 0 represents a dead cell.
+
+```javascript
+
+population = tf.randomUniform([height, width, 1], 0, 1, tf.int32);
+population = population.round();
+
+```
+
+The above code creates a population tensor using `tf.randomUniform`. This function creates a tensor with the specified shape and fills it with random values between 0 and 1. We then round the values to the nearest integer. This gives us a grid of cells where each cell is either alive or dead.
+
+## The Rules
+
+Once we have a grid to represent the state of each cell, we need to implement the rules of Conway's Game of Life. In TensorFlowJS, we can use the `conv2d` function to calculate the number of live neighbors for each cell. We can then use cascading logical operations to apply the rules to each cell.
+
+```javascript
+
+// Create a kernel for convolution
+let kernel = tf.ones([3, 3, 1, 1]);
+
+// Perform the convolution
+let convolvedPopulation = tf.conv2d(population, kernel, 1, 'same');
+
+let neighbors = tf.sub(convolvedPopulation, population);
+
+```
+
+The above code creates a kernel for convolution using `tf.ones`. This kernel will be used to calculate the number of live neighbors for each cell. We then use the `conv2d` function to perform the convolution and subtract the original population tensor from the result. This gives us a tensor where each cell represents the number of live neighbors for the corresponding cell in the original population tensor.
+
+We can then use cascading logical operations to apply the rules of Conway's Game of Life to each cell.
+
+```javascript
+
+// Check if a cell was alive in the previous generation
+let wasAlive = tf.equal(newPopulation, 1);
+
+// Check if a cell has two live neighbors
+let twoLiveNeighbors = tf.equal(neighbors, 2);
+
+// Check if a cell has three live neighbors
+let threeLiveNeighbors = tf.equal(neighbors, 3);
+
+// Apply the rules of Conway's Game of Life
+let finalPop = tf.logicalOr(threeLiveNeighbors, tf.logicalAnd(wasAlive, twoLiveNeighbors));
+
+```
+
+The above code uses the `tf.equal` function to check if a cell was alive in the previous generation, and if a cell has two or three live neighbors. We then use the `tf.logicalOr` and `tf.logicalAnd` functions to apply the rules of Conway's Game of Life to each cell.
+
+## Rendering the Grid
+
+Once we have applied the rules of Conway's Game of Life to each cell, we can render the grid to the screen. In TensorFlowJS, we can use the `tf.browser.toPixels` function to convert the population tensor to an image and display it on the screen.
+
+```javascript
+
+// Render the population tensor to the canvas
+tf.browser.toPixels(finalPop.toFloat(), document.getElementById('canvas'));
+
+```
+
+---
+
+__**Note:**__ Pay special care to the width, height, and speed of the grid. Because the calculations are performed on the GPU, the grid can be much larger and the calculations can be performed much faster than in the previous implementation. However, the speed of the grid will depend on the capabilities of the GPU. If you are using a device with a less powerful GPU, you may need to reduce the size of the grid to maintain a reasonable frame rate.
+
+---
+
+## Conclusion
+
+In this post, I reimplemented Conway's Game of Life using TensorFlowJS. This allowed me to use the GPU to perform the calculations, which should be much faster than using a 2D array and updating an HTML table. This implementation should be much more efficient for larger grids, and it should be able to maintain a reasonable frame rate even on less powerful devices.
+
+You can see a live demo of this implementation <a href='joshgracie.com/demos/conwaytensordemo'>here</a>. The game will start with a random grid, and you can adjust the speed and 'resolution' (pop size) of the grid.
+
+## References
+
+- [TensorFlowJS](https://www.tensorflow.org/js)
+- [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+- [Live Demo](https://joshgracie.com/demos/conwaytensordemo)
+- [ConwayLife](https://github.com/nrudakov/ConwayLife/blob/master/ConwayLife.py): Code was partially inspired by this implementation from nrudakov.
